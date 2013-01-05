dc9f27dfb7e72aaa21e688f67b1f3f954def599e
Michael Chapman <mc@mike.home.gateway>
2013-01-05 20:13:25 +1100

    Added 2f background and loader

--- alem.py
+++ alem.py
@@ -4,15 +4,52 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 
+from panda3d.core import Point2, Point3, Texture
+
 from player import Player
+
+app = None
+
+def load_object(tex = None, pos = Point2(0,0), depth = 0, transparency = True, scaleX = 1, scaleY = 1, scale = None):
+    global app
+    obj = app.loader.loadModel("models/plane")
+    obj.reparentTo(app.render)
+
+    obj.setPos(Point3(pos.getX(), pos.getY(), depth))
+
+    if (scale == None):
+        obj.setScale(scaleX, 1, scaleY)
+    else:
+        obj.setScale(scale)
+
+    obj.setBin("unsorted", 0) # ignore draw order (z-fighting fix)       
+    obj.setDepthTest(True)   # Don't disable depth write like the tut says
+    obj.setHpr(0, -90, 0)
+
+    if transparency:
+        obj.setTransparency(1) #All of our objects are transparent
+    else:
+        obj.setTransparency(0) #All of our objects are transparent
+    if tex:
+        tex = app.loader.loadTexture("textures/"+tex+".png") #Load the texture
+        tex.setWrapU(Texture.WMClamp)                    # default is repeat, which will give
+        tex.setWrapV(Texture.WMClamp)                    # artifacts at the edges
+        obj.setTexture(tex, 1)                           #Set the texture
+
+    return obj
 
 class Alem(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        global app
+        app = self
+
         self.scene = []
         self.player = Player(self)
         self.scene.append(self.player)
+
+        self.backgrounds = self.gen_background()
 
         self.rl = base.camLens.makeCopy()
 
@@ -38,11 +75,24 @@ class Alem(ShowBase):
         self.accept("s-up", self.player.move_down, [False])
 
     def update(self, task):
-        self.camera_task()
         for entity in self.scene:
             entity.update(task.time)
+        self.camera_task()
         return Task.cont
 
+    def gen_background(self):
+        bg_x = 5
+        bg_y = 5
+
+        tile_count = 10
+
+        backgrounds = []
+
+        for i in range(10):
+            for j in range(10):
+                backgrounds.append(load_object("grass", pos = Point2(i*bg_x, j*bg_y), transparency = False, scale = 5))
+
+        return backgrounds
 
 if __name__ == "__main__":
     print "works"
--- player.py
+++ player.py
@@ -7,22 +7,25 @@ class Player():
     up_move = False
     down_move = False
 
+    move_speed = 0.1
+
     def __init__(self, app):
         self.position = Point2(0,0)
         self.health = 100
         self.model = app.loader.loadModel('models/box')
+        self.model.setScale(0.5)
         self.model.reparentTo(app.render)
         self.model.setPos(self.position.x, self.position.y, 0)
 
     def update(self, timer):
         if self.left_move:
-            self.position.x = self.position.x - 1
+            self.position.x = self.position.x - self.move_speed
         if self.right_move:
-            self.position.x = self.position.x + 1
+            self.position.x = self.position.x + self.move_speed
         if self.up_move:
-            self.position.y = self.position.y + 1
+            self.position.y = self.position.y + self.move_speed
         if self.down_move:
-            self.position.y = self.position.y - 1
+            self.position.y = self.position.y - self.move_speed
         print self.position
         self.model.setPos(self.position.x, self.position.y, 0)
 
